678a09ef8e86c9debcd5b800b1a101459265f904
vitor.silva <vitor.silva@azi.com.br>
2022-05-14 08:34:36 -0400

    Pagination layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,11 +6,13 @@ import {
 import Listing from 'pages/Listing';
 import Form from 'pages/Form';
 import Navbar from "components/Navbar";
+import Pagination from "components/pagination";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <Pagination></Pagination>
       <Routes>
         <Route path="/" element={<Listing />} />
         <Route path="/form">
