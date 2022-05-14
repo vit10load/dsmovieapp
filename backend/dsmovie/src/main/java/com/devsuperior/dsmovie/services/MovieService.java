bf6221fb4ded32a606db5c823b5962ab1252d1f2
vitor.silva <vitor.silva@azi.com.br>
2022-05-14 10:39:00 -0400

    find movies

---
+++ backend/dsmovie/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
@@ -0,0 +1,29 @@
+package com.devsuperior.dsmovie.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.entity.Movie;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+
+@Service
+public class MovieService {
+
+	@Autowired
+	private MovieRepository movieRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<MovieDTO> findAll(Pageable pageable){
+		
+		Page<Movie> movies = movieRepository.findAll(pageable);
+		
+		Page<MovieDTO> moviDTOPage = movies.map(movie -> new MovieDTO(movie));
+		
+		return moviDTOPage;
+		
+	}
+}
